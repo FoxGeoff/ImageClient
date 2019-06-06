e003ef75f478e4fed4dabc0ad8d5d605747e0c11
FoxGeoff <fox.geoffrey@gmail.com>
2019-06-06 13:44:35 -0700

    Add Heroes list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { FormsModule } from '@angular/forms';
 
+import { InMemoryDataService } from './services/in-memory-data.service';
+
+import { RequestCache, RequestCacheWithMap } from './services/request-cache.service';
+     
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessageService } from './services/message.service';
+import { HeroesService } from './heroes/heroes.service';
+import { HttpErrorHandler } from './services/http-error-handler.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    //Always import the HttpClientInMemoryWebApiModule after the
+    //HttpClientModule to ensure that the in-memory backend provider 
+    //supersedes the Angular version.
+    environment.production ?
+    [] : HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
-  providers: [],
+  providers: [
+    HttpErrorHandler,
+    MessageService,
+    { provide: RequestCache, useClass: RequestCacheWithMap },
+    HeroesService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/in-memory-data.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+/*
+@Injectable({
+  providedIn: 'root'
+})
+*/
+@Injectable()
+export class InMemoryDataService implements InMemoryDbService {
+
+  constructor() { }
+
+  createDb() {
+    let heroes = [
+      { id: 1, name: 'Windstorm' },
+      { id: 2, name: 'Bombasto' },
+      { id: 3, name: 'Magneta' },
+      { id: 4, name: 'Tornado' }
+    ];
+    return { heroes };
+  }
+}
